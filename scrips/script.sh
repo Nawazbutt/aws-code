da965fcc816fc26eb0d0b7763ffad2ca7a839194
Muhammad Nawaz <buttnawaz919@gmail.com>
2022-01-10 11:11:54 +0500

    php

--- scrips/script.sh
+++ scrips/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -qy git curl libmcrypt-dev mysql-client
+apt-get update && apt-get install -qy git curl libmcrypt-dev mysql-client php7.4-common php7.4-mysql php7.4-cgi
+php7.4-mbstring php7.4-curl php7.4-gd php7.4-xml php7.4-xmlrpc php7.4-pear
 apt install mcrypt-1.0.1
 docker-php-ext-install pdo_mysql
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
