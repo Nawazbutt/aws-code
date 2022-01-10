d98c494de06363401d256b244169954715c52e99
Muhammad Nawaz <buttnawaz919@gmail.com>
2022-01-10 12:20:05 +0500

    npm

--- scrips/script.sh
+++ scrips/script.sh
@@ -3,3 +3,4 @@ apt-get update && apt-get install -qy git curl libmcrypt-dev mysql-client wget
 apt install mcrypt-1.0.1
 docker-php-ext-install pdo_mysql
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+apt -qy install npm
